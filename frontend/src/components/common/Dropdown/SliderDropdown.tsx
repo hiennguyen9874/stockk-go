a768761bcf02f67e9eab113fed2626557ea2a547
Xuan Hien <hiennguyen9874@gmail.com>
2023-03-29 00:17:58 +0700

    Update watchlist

--- frontend/src/components/common/Dropdown/SliderDropdown.tsx
+++ frontend/src/components/common/Dropdown/SliderDropdown.tsx
@@ -5,17 +5,20 @@ import cx from 'classnames';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
-interface SliderDropdownProps {
+import {
+  AddIcon,
+  WatchListIcon,
+  RemoveIcon,
+  EditIcon,
+} from 'components/common/Icon';
+
+interface WatchListsProps {
   currentItem: string;
   items: string[];
   onChange: (item: string) => void;
 }
 
-const SliderDropdown: FC<SliderDropdownProps> = ({
-  currentItem,
-  items,
-  onChange,
-}) => {
+const WatchLists: FC<WatchListsProps> = ({ currentItem, items, onChange }) => {
   return (
     <div className="w-full h-full text-right bg-slate-800">
       <Menu as="div" className="w-full h-full relative inline-block text-left">
@@ -51,32 +54,72 @@ const SliderDropdown: FC<SliderDropdownProps> = ({
         >
           <Menu.Items
             className={cx(
-              'absolute right-0 mt-1 w-full origin-top-right',
+              'absolute right-0 mt-1 px-1.5 w-full origin-top-right',
+              'divide-y divide-white divide-opacity-20',
               'rounded-md shadow-lg',
               'bg-slate-700',
               'ring-1 ring-black ring-opacity-5',
               'focus:outline-none'
             )}
           >
-            <div className="px-2 py-1 ">
+            <div className="py-1">
               {items.map((item) => (
                 <Menu.Item key={item}>
                   {({ active }) => (
                     <button
                       type="button"
                       className={cx(
-                        'group flex w-full items-center rounded-sm px-2 py-2 text-sm font-sans font-normal text-gray-100',
+                        'group flex w-full justify-between items-center rounded-sm px-2 py-2 text-sm font-sans font-normal text-gray-100',
                         {
                           'bg-slate-600': active || currentItem === item,
                         }
                       )}
                       onClick={() => onChange(item)}
                     >
-                      {item}
+                      <div className="flex flex-row items-center justify-center mr-auto">
+                        <div className="pr-1.5 pt-[2px]">
+                          <WatchListIcon />
+                        </div>
+                        <input
+                          type="text"
+                          className="bg-transparent border-none focus:border-none focus:outline-none"
+                          value={item}
+                          disabled
+                        />
+                      </div>
+                      <div className="flex flex-row items-center justify-center">
+                        <div className="mx-1 px-0.5 py-0.5 bg-blue-500">
+                          <EditIcon />
+                        </div>
+                        <div className="mx-1 px-0.5 py-0.5 bg-red-500">
+                          <RemoveIcon />
+                        </div>
+                      </div>
                     </button>
                   )}
                 </Menu.Item>
               ))}
+            </div>
+            <div className="py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    type="button"
+                    className={cx(
+                      'group flex flex-row w-full items-center rounded-sm px-2 py-2 text-sm font-sans font-normal text-gray-100',
+                      {
+                        'bg-slate-600': active,
+                      }
+                    )}
+                    onClick={() => {}}
+                  >
+                    <div className="pr-1 pt-[2px]">
+                      <AddIcon />
+                    </div>
+                    Tạo watchlist mới
+                  </button>
+                )}
+              </Menu.Item>
             </div>
           </Menu.Items>
         </Transition>
@@ -85,4 +128,4 @@ const SliderDropdown: FC<SliderDropdownProps> = ({
   );
 };
 
-export default memo(SliderDropdown);
+export default memo(WatchLists);
