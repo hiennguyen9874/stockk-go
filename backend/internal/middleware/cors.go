b7182475aa8087e7b2b40d5ef58cf1c5e61da8ab
Xuan Hien <hiennguyen9874@gmail.com>
2023-04-01 07:24:34 +0000

    Fix cors

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -8,13 +8,15 @@ func (mw *MiddlewareManager) Cors() cors.Options {
 	// Basic CORS
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
 	return cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		// AllowedOrigins: []string{"*"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"http://localhost:3000"},
+		// AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:     []string{"Link"},
+		AllowCredentials:   true,
+		OptionsPassthrough: false,
+		MaxAge:             300, // Maximum value not ignored by any of major browsers
+		Debug:              false,
 	}
 }
